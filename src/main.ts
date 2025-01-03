fd7957c2183ee697df884f546175dec3059f23c6
Jinyoung <dev.edchoi@gmail.com>
2025-01-03 15:43:18 +0900

    ing...

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,33 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue/dist/vue.esm-bundler.js'
 
-const App = {
-    render() {
-        return "this is the app's entrance";
-    }    
+// const App = {
+//     template: "This is the app's entrance",
+// }
+
+// const App = {
+//     render() {
+//         return "This is the app's entrance"
+//     } 
+// }
+
+// const Description = {
+//     template: "This is the app's entrance",
+// }
+
+// const App = {
+//     components: { Description },
+//     template: '<Description />'
+// }
+
+// const App = {    
+//     template: "<div>This is te apps entrance</div>"
+// }
+
+const App = {    
+    template : `
+        <h1>This is the app's entrance</h1>
+        <h2>we are exploring template syntax</h2>
+    `,
 }
 
 const app = createApp(App)
